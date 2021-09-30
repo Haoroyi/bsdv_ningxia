7ff76e62e590374a3a943669eecc911502b00eeb
Haoyi <haoroyi@126.com>
2021-09-30 16:24:36 +0800

    map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,29 +1,82 @@
 <template>
-  <div id="map"></div>
+  <!-- <chart ref="chart1" :options="orgOptions" :auto-resize="true"></chart> -->
+  <div id="myChart"></div>
 </template>
 
 <script>
+import * as echarts from 'echarts'
+import ningxiaMap from '../utils/map/ningxia.json'
+const mapData = [{ name: '固原市', value: 40 }]
 export default {
   name: 'Map',
-  data () {
+  data() {
     return {
-      polar: {
-        // 指定图表的配置项和数据
+      option: {
+        geo: {
+          type: 'map',
+          map: 'YN',
+          top: 20,
+          itemStyle: {
+            areaColor: '#045aff',
+            borderColor: '#448bf6'
+          },
+          emphasis: {
+            itemStyle: {
+              // 高亮时点的颜色。
+              color: '#ffb628'
+            },
+            label: {
+              show: true,
+              // 高亮时标签的文字。
+              formatter: 'This is a emphasis label.',
+              color: '#fff'
+            },
+            tooltip: {
+              show: true
+            }
+          }
+        },
+        series: [
+          {
+            name: '宁夏',
+            type: 'scatter',
+            coordinateSystem: 'geo',
+            data: mapData
+          }
+        ]
       }
     }
   },
   mounted() {
+    this.drawLine()
   },
   methods: {
-
+    drawLine() {
+      // 基于准备好的dom，初始化echarts实例
+      const myChart = echarts.init(document.getElementById('myChart'))
+      echarts.registerMap('YN', { geoJSON: ningxiaMap })
+      // 绘制图表
+      myChart.setOption(this.option)
+      var index = 0 // 播放所在下标
+      setInterval(function () {
+        myChart.dispatchAction({
+          type: 'showTip',
+          seriesIndex: 0,
+          dataIndex: index
+        })
+        index++
+        if (index > mapData.length) {
+          index = 0
+        }
+      }, 1000)
+    }
   }
 }
 </script>
 
 <style>
-#map {
+#myChart {
   width: 100%;
   height: 100%;
-  /* background-color: yellow; */
 }
 </style>
---
+++ src/components/UseLogin.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="use-login"></div>
+</template>
+
+<script>
+export default {
+  name: 'UseLogin'
+}
+</script>
+
+<style>
+</style>
